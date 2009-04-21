65275b4e5e1aecc5ec538f0653ef6382719f54a7
James Willcox <snorp@novell.com>
2009-04-21 17:28:05 -0500

    oops, forgot some stuff

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,8 +1,7 @@
 module NavigationHelper
   def page_links_for_navigation
     link = Struct.new(:name, :url)
-    [link.new("Home", posts_path),
-     link.new("Archives", archives_path)] + 
+    [link.new("Blog", posts_path), link.new("photos", "http://flickr.com/photos/snorp")] +
       Page.find(:all, :order => 'title').collect {|page| link.new(page.title, page_path(page))}
   end
 
