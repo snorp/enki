2a485c8c30d839f83341998debdbc89634cb34a2
James Willcox <snorp@novell.com>
2009-04-21 17:28:05 -0500

    oops, forgot some stuff

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+  if (document.referrer.search(location.hostname) < 0) {
+    $("#navigation").effect('slide', {}, 500);
+  } else {
+    $("#navigation").show();
+  }
+});
