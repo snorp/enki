8d996d3f628add02c973a8a229561110e5500fa3
James Willcox <snorp@novell.com>
2009-04-24 15:52:46 -0500

    include mailer config in environment

--- config/environment.rb
+++ config/environment.rb
@@ -44,4 +44,7 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = YAML::load_file("#{RAILS_ROOT}/config/mailer.yml")
 end
